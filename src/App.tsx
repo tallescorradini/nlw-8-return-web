6d3b0c695a6b51a92d88240b2700ddb93b1ac812
Talles Corradini <tallescorradini@gmail.com>
2022-05-24 16:48:30 -0300

    refactor(Widget): accept popover button and pannel as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Popover } from "@headlessui/react";
 import { ChatTeardropDots } from "phosphor-react";
 import React from "react";
 import { feedbackTypes, Widget } from "./components";
@@ -9,34 +8,38 @@ import { WidgetForm } from "./components/Widget/components/WidgetForm/WidgetForm
 function App() {
   return (
     <>
-      <Widget>
-        <WidgetForm
-          header={
-            <WidgetHeader
-              title={
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-              }
-              closeButton={<CloseButton />}
-            />
-          }
-          firstStep={<FeedbackTypeStep feedbackTypes={feedbackTypes} />}
-          footerContent={
-            <span>
-              Feito com ♥ pela{" "}
-              <a
-                href="https://rocketseat.com.br"
-                className="underline underline-offset-2"
-              >
-                Rocketseat
-              </a>
-            </span>
-          }
-        />
-        <WidgetPopoverButton
-          icon={<ChatTeardropDots className="w-6 h-6" />}
-          text="Feedback"
-        />
-      </Widget>
+      <Widget
+        button={
+          <WidgetPopoverButton
+            icon={<ChatTeardropDots className="w-6 h-6" />}
+            text="Feedback"
+          />
+        }
+        pannel={
+          <WidgetForm
+            header={
+              <WidgetHeader
+                title={
+                  <span className="text-xl leading-6">Deixe seu feedback</span>
+                }
+                closeButton={<CloseButton />}
+              />
+            }
+            firstStep={<FeedbackTypeStep feedbackTypes={feedbackTypes} />}
+            footerContent={
+              <span>
+                Feito com ♥ pela{" "}
+                <a
+                  href="https://rocketseat.com.br"
+                  className="underline underline-offset-2"
+                >
+                  Rocketseat
+                </a>
+              </span>
+            }
+          />
+        }
+      />
     </>
   );
 }
@@ -72,12 +75,12 @@ interface WidgetPopoverButtonProps {
 
 function WidgetPopoverButton({ icon: Icon, text }: WidgetPopoverButtonProps) {
   return (
-    <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+    <>
       {Icon}
       <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
         <span className="pl-2"></span>
         {text}
       </span>
-    </Popover.Button>
+    </>
   );
 }
--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -38,14 +38,19 @@ export interface FeedbackTypeObject {
 export type FeedbackType = keyof typeof feedbackTypes;
 
 interface Props {
-  children: React.ReactNode;
+  button: React.ReactNode;
+  pannel: React.ReactNode;
 }
 
-export function Widget({ children }: Props) {
-  console.log(children);
+export function Widget({ button: Button, pannel: Pannel }: Props) {
   return (
     <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
-      {children}
+      <Popover.Panel className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        {Pannel}
+      </Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        {Button}
+      </Popover.Button>
     </Popover>
   );
 }
